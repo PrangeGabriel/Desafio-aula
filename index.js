87985b35f9798a66714f382ee9082955bd2655ac
Gabriel <prange.gabriel@gmail.com>
2021-03-08 21:29:31 -0300

    parte 2 da aula

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const path = require("path");
+
+//pega todos
+// https://pokeapi.co/api/v2/pokemon?offset=0&limit=251
+
+//pega nome do pokemon especifico do pokemon/
+// https://pokeapi.co/api/v2/pokemon/
+
+const api = axios.create({
+  baseURL: "https://pokeapi.co/api/v2/pokemon?offset=0&limit=251",
+});
+
+//rotas
+app.get("/pokemon", async (req, res) => {
+  const responseAxios = await api.get(
+    "https://pokeapi.co/api/v2/pokemon?offset=0&limit=251"
+  );
+  const { data } = responseAxios;
+  const retorno = {
+    ...data,
+    results: data.results.map((item) => {
+      item.greetings = "Olá";
+      return item;
+    }),
+  };
+  return res.json(retorno);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname,''));
+});
+
+app.listen(3000, () => {
+  console.log("servidor rodando na porta 3000!");
+  console.log("http://localhost:3000/");
+});
